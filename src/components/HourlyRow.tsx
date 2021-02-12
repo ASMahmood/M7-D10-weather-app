ae950d3414d52a2338fd8abd80d888e3f2cd3bdd
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-02-12 13:54:13 +0000

    installed owl carousel

--- src/components/HourlyRow.tsx
+++ src/components/HourlyRow.tsx
@@ -5,5 +5,13 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 export default function HourlyRow(props: apiStructure) {
-  return <div></div>;
+  return (
+    <div id="hourRow">
+      {props.hourly !== undefined && (
+        <OwlCarousel className="owl-theme" loop margin={10} nav>
+          <div>hi</div>
+        </OwlCarousel>
+      )}
+    </div>
+  );
 }
